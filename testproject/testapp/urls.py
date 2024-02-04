29ffc7dbf5ee471bb60e1dce9e45413a0ea2aded
imranaliget@gmail.com <imranaliget@gmail.com>
2024-02-04 11:05:49 +0500

    add the data

--- testproject/testapp/urls.py
+++ testproject/testapp/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from .views import*
 urlpatterns = [
     path('',index, name='index'),
-<<<<<<< HEAD
     path('about/', about, name='about'),
-=======
     path('about', about, name='about'),
-    path('contact',contact,name='contact')
->>>>>>> de4eed097d070f74875ceb87fe9be01a47c63f1b
+    path('contact',contact,name='contact'),
 ]
