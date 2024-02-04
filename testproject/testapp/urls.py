3827bffa822984efce8cb40351b99fc5a8a7d7c2
Rashidzada <rashidzada6@gmail.com>
2024-02-04 10:58:58 +0500

    all is fit

--- testproject/testapp/urls.py
+++ testproject/testapp/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from .views import*
 urlpatterns = [
     path('',index, name='index'),
-<<<<<<< HEAD
-    path('about/', about, name='about'),
-=======
     path('about', about, name='about'),
     path('contact',contact,name='contact')
->>>>>>> de4eed097d070f74875ceb87fe9be01a47c63f1b
 ]
