20a806155e11620f62186446e0a9e8b8b0f79d8e
Rashidzada <rashidzada6@gmail.com>
2024-02-04 10:42:34 +0500

    new changews

--- testproject/testapp/views.py
+++ testproject/testapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import *
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -7,4 +8,14 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
+    if request.method == ['POST']:
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        Contact.objects.create(name = name , email = email , subject = subject , message = message)
+        return redirect('contact')
+    else:
+        messages.warning(request,"You mesasage is not sent try again")
+
     return render(request,'contact.html')
